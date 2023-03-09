eb54348d8a74561b14b840d6dc6ce8f60ac1e480
jorgeGruver <jpedroza@gruver.mx>
2023-03-08 22:17:37 -0600

    first commit

---
+++ prueba nuevos/src/components/ModalPrueba.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-dialog v-model="abrirModal">
+    <q-card class="full-width">
+      <q-card-section class="bg-primary text-white row items-center q-pb-none">
+
+        <h2 class="text-h6" v-if="contador > 10">Tu contador es mayor que 10</h2>
+        
+        <h2 class="text-h6" v-else>Tu contador es menor a 10</h2>
+        
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
+      </q-card-section>
+      <q-card-section>
+        <div>Tu contador es: {{ contador }} </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+import { ref, } from 'vue'
+
+export default {
+  setup() {
+
+    const abrirModal = ref(false)
+    const contador = ref(0)
+
+    const abrir = (cuentaActual) => {
+      contador.value = cuentaActual
+      abrirModal.value = true
+    }
+
+    return {
+      abrirModal,
+      abrir,
+      contador
+    }
+  },
+}
+</script>
+
+<style></style>
---
+++ prueba nuevos/src/pages/IndexPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-page class="flex flex-center">
+
+    <div class="column">
+      
+      <q-btn color="primary" icon="add" @click="sumarContador" />
+      <q-btn class="q-mt-md" color="primary" icon="clear" @click="contador = 0" />
+
+      <div align="center">
+        <div class="q-mt-xl text-h2"> {{ contador }}</div>
+      </div>
+
+      <div class="q-mt-xl">
+        <q-btn color="primary" label="Abrir modal" icon="open_with" @click="abrirModal" />
+      </div>
+
+    </div>
+
+  </q-page>
+
+  <ModalPrueba ref="modal"></ModalPrueba>
+</template>
+
+<script>
+import { ref } from 'vue'
+import ModalPrueba from 'src/components/ModalPrueba.vue'
+
+export default {
+  components: {
+    ModalPrueba
+  },
+
+  setup() {
+    const contador = ref(0)
+    const modal = ref(null)
+
+    const sumarContador = () => {
+      contador.value++
+      console.log(contador.value)
+    }
+
+    const abrirModal = () => {
+      modal.value.abrir(contador.value)
+    }
+
+    return {
+      contador,
+      sumarContador,
+      abrirModal,
+      modal
+    }
+
+  },
+}
+</script>
+
+<style>
+
+</style>
